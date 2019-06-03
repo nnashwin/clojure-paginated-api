a2428ae355e95580a583255cd04968f15fc48d19
Tyler Boright <lupin3.ken@gmail.com>
2019-06-03 13:04:02 -0700

    adds ring-core and ring-jetty to the project

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/java.jdbc "0.7.9"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [mysql/mysql-connector-java "8.0.16"]]
   :main ^:skip-aot paginated-api.core
