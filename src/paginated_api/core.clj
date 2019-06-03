14040b038d896dd43b94a82180c0b460c752c91b
Tyler Boright <lupin3.ken@gmail.com>
2019-06-03 13:04:23 -0700

    adds webserver implementation logic to the core package

--- src/paginated_api/core.clj
+++ src/paginated_api/core.clj
@@ -1,7 +1,24 @@
 (ns paginated-api.core
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.edn :as edn])
+  (:use ring.adapter.jetty)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+;; Read in config for db
+;; Check out http://clojure-doc.org/articles/ecosystem/java_jdbc/home.html for the structure of the db-spec
+(def db-spec
+  (edn/read-string (slurp "data/db-config.edn")))
+
+(defn print-db-connection
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println "It's the next line")
+  (println (jdbc/query db-spec ["SELECT 3*5 AS result"])))
+
+(defn handler [req]
+  (print-db-connection)
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body "Yo Dude!"})
+
+(run-jetty handler {:port 8675})
