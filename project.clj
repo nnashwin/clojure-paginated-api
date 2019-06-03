4fe7b403029a5c0be4fabf5096a5547085afaabd
Tyler Boright <lupin3.ken@gmail.com>
2019-06-03 12:14:52 -0700

    adds the correct msql-connector-java dependency to the project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/java.jdbc "0.7.9"]]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [mysql/mysql-connector-java "8.0.16"]]
   :main ^:skip-aot paginated-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
